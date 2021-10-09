f32e859f08b2dc960da6acbeb7a682db2b818a3d
melghosh <91874965+melghosh@users.noreply.github.com>
2021-10-09 19:51:31 +0200

    topic labeling

--- Main2.py
+++ Main2.py
@@ -11,8 +11,12 @@
 
 import TopicLabeling
 
-if __name__ == "__main__":
-    #apply topic labeling
+def labeling():
+    # apply topic labeling
     t = TopicLabeling.TopicLabeling()
     t.labeling_process('ontology/Topic-OPA.owl', '')
-    #t.labeling_process('ontology/Topic-RPA.owl', 'recent-press/')
+    # t.labeling_process('ontology/Topic-RPA.owl', 'recent-press/')
+
+if __name__ == "__main__":
+    labeling()
+
